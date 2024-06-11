642d9765cf4e8493c57c93c5f3dca250c1b11c0e
Sagar2102 <sagar20032610@gmail.com>
2024-06-11 12:58:45 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,12 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes  from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+
 const app=express();
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+app.use(express.json());
 
 app.get("/", (req, res) => {
 // root route http://localhost:5000/
